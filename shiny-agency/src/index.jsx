1a74d8a787da9eb0f35a17624ca2fac264357561
Laurent Sauzet <laurent.sauzetpro@gmail.com>
2022-06-02 11:43:22 +0200

    Transformez votre application en SPA avec React Router

--- shiny-agency/src/index.jsx
+++ shiny-agency/src/index.jsx
@@ -1,13 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './index.css';
 import Home from './pages/Home/Index';
 import reportWebVitals from './reportWebVitals';
+import Survey from './pages/Survey'
+import Results from './pages/Results'
+import Freelances from './pages/Freelances'
+import Header from './components/Header'
+import Error from './components/Error'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home />
+     <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/survey/:questionNumber">
+          <Survey />
+        </Route>
+        <Route path="/results">
+          <Results />
+        </Route>
+        <Route path="/freelances">
+          <Freelances />
+        </Route>
+        <Route>
+          <Error />
+        </Route>
+      </Switch>
+    </Router>
   </React.StrictMode>,
 );
 
